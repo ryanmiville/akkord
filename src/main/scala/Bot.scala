e61f17b381963925d027c2b20db29a601a7ef1da
ryanmiville <ryanmiville@gmail.com>
2017-08-05 13:30:36 -0400

    discord bot clean up

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -5,7 +5,7 @@ class Bot(token: String) extends DiscordBot(token) {
 
   val channel = system.actorOf(ChannelApi.props(token))
 
-  override def receiveFromDiscord = {
+  override def botBehavior = {
     case MessageCreated(id, "ping") =>
       channel ! Message(id, "pong")
       sender ! Ack
