21b9dd21c41a153fc83f160bd4e9b7de3ade0ebd
ryanmiville <ryanmiville@gmail.com>
2017-08-06 00:28:52 -0400

    pulled out websocket connecting

--- src/main/scala/akkord/DiscordBot.scala
+++ src/main/scala/akkord/DiscordBot.scala
@@ -1,74 +1,36 @@
 package akkord
 
-import akka.actor.{Actor, Props}
+import akka.actor.Actor
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{HttpRequest, HttpResponse, StatusCodes}
-import akka.http.scaladsl.model.ws.{TextMessage, WebSocketRequest}
-import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.pattern.ask
+import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.ws.TextMessage
 import akka.pattern.pipe
-import akka.stream.scaladsl.{Keep, Sink, Source, SourceQueueWithComplete}
-import akka.stream.{ActorMaterializer, KillSwitches, OverflowStrategy, UniqueKillSwitch}
-import akka.util.Timeout
-import io.circe.Json
-import parser.PayloadParser
-import api.DiscordApi._
+import akkord.WebsocketConnectionBehavior._
+import akkord.api.DiscordApi._
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+import io.circe.Json
 
 import scala.concurrent.duration._
 
-abstract class DiscordBot(token: String) extends Actor with FailFastCirceSupport{
+abstract class DiscordBot(token: String) extends Actor
+  with WebsocketConnectionBehavior with FailFastCirceSupport {
   import DiscordBot._
 
-  protected implicit val ec           = context.system.dispatcher
-  protected implicit val system       = context.system
-  protected implicit val materializer = ActorMaterializer()
-  protected implicit val timeout      = Timeout(10 seconds)
-
-  private val payloadParser = system.actorOf(Props(classOf[PayloadParser], self))
-
-  private var connectionUrl = ""
   private var lastSeq: Option[Int] = None
   private var sessionId = ""
 
-  var queue: SourceQueueWithComplete[DiscordBot.WsMessage] = null
-  var killswitch: UniqueKillSwitch = null
-
   override def preStart() = {
-    Http().singleRequest(HttpRequest(uri = s"$baseUrl/gateway"))
+    Http(system).singleRequest(HttpRequest(uri = s"$baseUrl/gateway"))
       .pipeTo(self)
   }
 
   override def receive: Receive = connecting
 
-  private def connecting: Receive = {
-    case HttpResponse(StatusCodes.OK, _, entity, _) =>
-      import io.circe.generic.auto._
-      Unmarshal(entity)
-        .to[Connection]
-        .pipeTo(self)
-    case Connection(url) =>
-      connectionUrl = url
-      context become connected
-      val (q, ks) =
-        Source.queue[WsMessage](Int.MaxValue, OverflowStrategy.dropBuffer)
-          .via(Http().webSocketClientFlow(WebSocketRequest(s"$url?v=6&encoding=json")))
-          .viaMat(KillSwitches.single)(Keep.both)
-          .collect { case tm: TextMessage.Strict => tm }
-          .mapAsync(10)(msg => (payloadParser ? msg).mapTo[GatewayPayload])
-          .to(Sink.actorRefWithAck(self, Init, Ack, Complete))
-          .run()
-      queue = q
-      killswitch = ks
-    case resp @ HttpResponse(code, _, _, _) =>
-      println(s"Failed to retrieve URL: $code")
-  }
-
-  private def connected: Receive =
+  override def connected: Receive =
     botBehavior orElse
-      receiveStreamPlumbing orElse
-      receiveGatewayPayload orElse
-      { case _ => sender ! Ack }
+    receiveStreamPlumbing orElse
+    receiveGatewayPayload orElse
+    { case _ => sender ! Ack }
 
   private def receiveStreamPlumbing: Receive = {
     case Init       => sender ! Ack
@@ -106,12 +68,6 @@ abstract class DiscordBot(token: String) extends Actor with FailFastCirceSupport
 }
 
 object DiscordBot {
-  case class Connection(url: String)
-
-  private case object Init
-  case object Ack
-  private case object Complete
-
   case object Disconnect
 
   trait GatewayPayload
@@ -128,8 +84,6 @@ object DiscordBot {
 
   case object HeartBeat
   case class NewSeq(s: Int)
-
-  private type WsMessage = akka.http.scaladsl.model.ws.Message
 
   private def identify(token: String) = TextMessage(
     s"""{
---
+++ src/main/scala/akkord/WebsocketConnectionBehavior.scala
@@ -0,0 +1,74 @@
+package akkord
+
+import akka.actor.{Actor, Props}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.ws.{Message, TextMessage, WebSocketRequest}
+import akka.http.scaladsl.model.{HttpResponse, ResponseEntity, StatusCodes}
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.pattern.{ask, pipe}
+import akka.stream._
+import akka.stream.scaladsl.{Keep, Sink, Source, SourceQueueWithComplete}
+import akka.util.Timeout
+import akkord.DiscordBot.GatewayPayload
+import akkord.parser.PayloadParser
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+
+import scala.concurrent.duration._
+
+trait WebsocketConnectionBehavior {
+  this: Actor  with FailFastCirceSupport =>
+  import WebsocketConnectionBehavior._
+
+  var connectionUrl: Option[String] = None
+
+  var queue: SourceQueueWithComplete[Message] = _
+  var killswitch: UniqueKillSwitch            = _
+
+  val system = context.system
+
+  protected implicit val ec           = system.dispatcher
+  protected implicit val materializer = ActorMaterializer()
+  protected implicit val timeout      = Timeout(10 seconds)
+
+  private val payloadParser = system.actorOf(Props(classOf[PayloadParser], self))
+
+  def connecting: Receive = {
+    case HttpResponse(StatusCodes.OK, _, entity, _) => pipeConnection(entity)
+    case Connection(url)                            => connect(url)
+  }
+
+  protected def connect(url: String) = {
+    context become connected
+    connectionUrl = Some(url)
+    val (q, ks)   = runStream(url)
+    queue         = q
+    killswitch    = ks
+  }
+
+  private def runStream(url: String): (SourceQueueWithComplete[Message], UniqueKillSwitch) = {
+    Source.queue[Message](Int.MaxValue, OverflowStrategy.dropBuffer)
+      .via(Http(context.system).webSocketClientFlow(WebSocketRequest(s"$url?v=6&encoding=json")))
+      .viaMat(KillSwitches.single)(Keep.both)
+      .collect { case tm: TextMessage.Strict => tm }
+      .mapAsync(10)(msg => (payloadParser ? msg).mapTo[GatewayPayload])
+      .to(Sink.actorRefWithAck(self, Init, Ack, Complete))
+      .run()
+  }
+
+  private def pipeConnection(entity: ResponseEntity) = {
+    import io.circe.generic.auto._
+    Unmarshal(entity)
+      .to[Connection]
+      .pipeTo(self)
+  }
+
+  def connected: Receive
+}
+
+object WebsocketConnectionBehavior {
+  case object Init
+  case object Ack
+  case object Complete
+
+  case class Connection(url: String)
+}
